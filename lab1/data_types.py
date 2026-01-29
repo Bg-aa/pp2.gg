e7ca0f3229dceb121f5c68228aade2bf137605cf
Bakonya <230343404+Bg-aa@users.noreply.github.com>
2026-01-29 19:39:41 +0500

    w3school examples

---
+++ lab1/data_types.py
@@ -0,0 +1,24 @@
+# Python Data Types
+#Variables can store data of different types, and different types can do different things.
+""" Texting Type - str
+    Numeric Types - int, float, complex 
+    Sequence Types - list,tuple,range
+    Mapping Type - dict 
+    Set Types - set,frozenset
+    Boolean Type - bool
+    Binary Types - bytes,bytearray,memoryview 
+    None Type - NoneType """
+# ex1:
+x = ["apple", "banana", "cherry"]  #list 
+
+#ex2:
+x = ("apple", "banana", "cherry")  #tuple
+
+#ex3:
+x = range(6)  #range
+
+#ex4:
+x = {"name" : "John", "age" : 36}  #dict
+
+#ex5:
+x = True #bool
