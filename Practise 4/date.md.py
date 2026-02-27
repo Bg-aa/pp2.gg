cbb60290233de8a58af987e4e802220252b4dfd5
Bakonya <230343404+Bg-aa@users.noreply.github.com>
2026-02-27 12:32:25 +0500

    prac4

--- Practise 4/date.md.py
+++ Practise 4/date.md.py
@@ -1,38 +1,37 @@
-import math
+#1
+from datetime import datetime, timedelta
 
-R = float(input())
-x1, y1 = map(float, input().split())
-x2, y2 = map(float, input().split())
+current_date = datetime.now()
+new_date = current_date - timedelta(days=5)
 
-def length(p1, p2):
-    return math.hypot(p1[0]-p2[0], p1[1]-p2[1])
+print("Current date:", current_date)
+print("5 days ago:", new_date)
+#2
+from datetime import datetime, timedelta
 
-A = (x1, y1)
-B = (x2, y2)
+today = datetime.now()
+yesterday = today - timedelta(days=1)
+tomorrow = today + timedelta(days=1)
 
-dx, dy = B[0]-A[0], B[1]-A[1]
+print("Yesterday:", yesterday)
+print("Today:", today)
+print("Tomorrow:", tomorrow)
+#3
+from datetime import datetime
 
-dist = abs(dx*A[1] - dy*A[0]) / math.hypot(dx, dy)
+now = datetime.now()
+without_microseconds = now.replace(microsecond=0)
 
-if dist >= R:
+print("Before:", now)
+print("After:", without_microseconds)
 
-    print(f"{length(A,B):.10f}")
-else:
+#4
+from datetime import datetime
 
-    def tangent_length(P):
-        return math.sqrt(P[0]**2 + P[1]**2 - R**2)
-    
-    tA = tangent_length(A)
-    tB = tangent_length(B)
+date1 = datetime(2026, 2, 20, 12, 0, 0)
+date2 = datetime(2026, 2, 25, 15, 30, 0)
 
-    angleA = math.atan2(A[1], A[0])
-    angleB = math.atan2(B[1], B[0])
+difference = date2 - date1
+seconds = difference.total_seconds()
 
-    alphaA = math.asin(R / math.hypot(A[0], A[1]))
-    alphaB = math.asin(R / math.hypot(B[0], B[1]))
-    
-    theta = abs((angleB - alphaB) - (angleA + alphaA))
-    theta = (theta + 2*math.pi) % (2*math.pi)  
-    
-    L = tA + tB + R*theta
-    print(f"{L:.10f}")
+print("Difference in seconds:", seconds)
