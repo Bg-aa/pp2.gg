2b726f422cc4e4b1e30af1bcd8f811a8c9f12fb7
Bakonya <230343404+Bg-aa@users.noreply.github.com>
2026-02-27 12:31:53 +0500

    prac4

--- main1.py
+++ main1.py
@@ -1,15 +1,4 @@
-from datetime import datetime, timedelta
-def parse(line):
-    date_part, tz_part = line.split()
-    dt = datetime.strptime(date_part, "%Y-%m-%d")
-    sign = 1 if "+"in tz_part else -1
-    hours, minutes = map(int, tz_part[4:].split(":"))
-    offset = timedelta(hours=hours, minutes=minutes)
-    offset *= sign 
-    return dt - offset
-line1 = input().split()
-line2 = input().split()
-dt1 = parse(line1)
-dt2 = parse(line2)
-difference = abs((dt1 - dt2).days)
-print(difference) 
+import json
+json_data = '{"name": "Alice", "age": 30, "city": "New York"}'
+data = json.loads(json_data)
+print(data["age"])
