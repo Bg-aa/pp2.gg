08ad1c81036fa56b8b2dadd3127c2f1620394cf9
Bakonya <230343404+Bg-aa@users.noreply.github.com>
2026-04-30 19:17:16 +0500

    TSIS

--- main.py
+++
@@ -1,8 +0,0 @@
-with open("nums.txt", "w") as file:
-    s = input()
-    file.write(s)
-with open("nums.txt", "r") as f:
-    nums = list(map(int,f.read().split()))
-with open("sum.txt", "w") as f:
-    f.write(nums)
-    print(sum(nums))
